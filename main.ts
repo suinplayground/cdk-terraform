ca3586dca4ca67e322b5996da877c0fc898c1995
suin <suinyeze@gmail.com>
2022-04-05 17:42:54 +0900

    feat: 🎸 add backend

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { App, RemoteBackend, TerraformOutput, TerraformStack } from "cdktf";
 import { Construct } from "constructs";
 import { GithubProvider } from "./.gen/providers/github";
 import { Repository } from "./.gen/providers/github/repository";
@@ -6,6 +6,16 @@ import { Repository } from "./.gen/providers/github/repository";
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
+
+    // Define Remote Backends
+    // https://www.terraform.io/cdktf/concepts/remote-backends
+    new RemoteBackend(this, {
+      hostname: "app.terraform.io",
+      organization: "suinplayground",
+      workspaces: {
+        name: "cdk-for-terraform-playground",
+      },
+    });
 
     // Enable github provider
     new GithubProvider(this, "github", {
